4f0b0d3c90f5abf563b369d29123c050ecbb4fad
Jose Gonzalez <josegonzalez293@gmail.com>
2021-07-23 21:08:48 -0400

    (:tada:) first commit

---
+++ api/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from search import search_video
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "API LISTEN ON PORT 5001!"
+
+@app.route('/search/<title>')
+def search(title):
+    videos = search_video(title)
+    return jsonify(videos)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
---
+++ api/src/search.py
@@ -0,0 +1,39 @@
+import os
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+API_KEY = os.getenv('API_KEY')
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+def search_video(title):
+  try: 
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=API_KEY)
+
+    response = youtube.search().list(
+      q=title,
+      part='id,snippet',
+      maxResults=20
+    ).execute()
+
+    videos = []
+    for result in response.get('items', []):
+      if result['id']['kind'] == 'youtube#video':
+
+        videoId = result['id']['videoId']
+        title = result['snippet']['title']
+        description = result['snippet']['description'][0:30]
+        image = result['snippet']['thumbnails']['default']
+        
+        videos.append({
+          "id": videoId, 
+          "title": title,
+          "description": description,
+          "thumbnail": image
+        })
+
+    return videos
+
+  except Exception as e:
+    print('An error has occurred: {}'.format(e))
+    return {"error": e}

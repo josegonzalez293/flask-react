ac6e298e59e72ef99b836962e2b14874fe9cbc72
Jose Gonzalez <josegonzalez293@gmail.com>
2021-07-23 22:38:19 -0400

    add init

--- api/src/app.py
+++ api/src/app.py
@@ -15,4 +15,4 @@ def search(title):
     return jsonify(videos)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=False, host='0.0.0.0', port=5001)
